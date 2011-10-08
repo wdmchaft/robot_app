6fb96610276ab73fa3442e45245e6936572a3914
Your Name <tropicdome@hotmail.com>
2011-10-08 03:35:02 +0200

    .

---
+++ Peegee/Peegee/ChatViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Peegee
+//
+//  Created by Simon Bøgh on 08/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatViewController : UIViewController
+
+@end
