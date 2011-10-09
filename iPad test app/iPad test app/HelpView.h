ce15b400fe38066ec9978c329041b681bbd3d779
Your Name <tropicdome@hotmail.com>
2011-10-10 01:55:43 +0200

    iPad test app - Initial layout

---
+++ iPad test app/iPad test app/HelpView.h
@@ -0,0 +1,13 @@
+//
+//  HelpView.h
+//  iPad test app
+//
+//  Created by Simon Bøgh on 10/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelpView : UIViewController
+
+@end
---
+++ iPad test app/iPad test app/SkillsLibView.h
@@ -0,0 +1,13 @@
+//
+//  SkillsLibView.h
+//  iPad test app
+//
+//  Created by Simon Bøgh on 10/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SkillsLibView : UIViewController
+
+@end
